5dff1bbfa43d70cc309d95403231218bd90cac3a
valerii.grigorev <valerii.grigorev@simbirsoft.com>
2020-12-02 12:10:08 +0400

    3-5 what is context?

--- lib/MyFirstStatelessWidget.dart
+++ lib/MyFirstStatelessWidget.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class MyFirstStatelessWidget extends StatelessWidget {
   int _count = 0;
+  late BuildContext _context;
 
   @override
   Widget build(BuildContext context) {
+    this._context = context;
     print('Stateless: ${++_count}');
     return Container(
-      child: Center(
-        child: Text('Hello! (Stateless)'),
-      ),
-    );
+        child: Column(
+      children: [
+        Text('Hello! (Stateless)'),
+        Container(
+          color: Colors.blue,
+          child: MaterialButton(
+            child: Text('Tap on me for print\ncontext.runtimeType'),
+            onPressed: () {
+              print(_getContextRuntimeType());
+            },
+          ),
+        )
+      ],
+    ));
+  }
+
+  Type _getContextRuntimeType() {
+    return _context.runtimeType;
   }
 }
