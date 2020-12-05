c8cfd7016c052e7e3f2adee40469ff4f8ec57841
valerii.grigorev <valerii.grigorev@simbirsoft.com>
2020-12-05 10:09:09 +0400

    4-2-1 use examples Text

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:places/texts.dart' as Texts;
+import 'package:places/texts.dart';
 import 'package:places/ui/screen/sight_list_screen.dart';
 
 void main() {
@@ -7,17 +7,16 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: Texts.APP_TITLE,
+      title: APP_TITLE,
       theme: ThemeData(
         primarySwatch: Colors.grey,
         fontFamily: 'Roboto',
       ),
-      home: SightListScreen(Texts.FIRST_SCREEN_TITLE),
+      home: SightListScreen(FIRST_SCREEN_TITLE),
     );
   }
 }
