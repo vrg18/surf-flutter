a52cb419ba14ddd9480b90ffb9c272149cf0b502
valerii.grigorev <valerii.grigorev@simbirsoft.com>
2020-12-05 11:02:43 +0400

    4-2-2 use examples RichText and TextSpan

--- lib/texts.dart
+++ lib/texts.dart
@@ -1,3 +1,9 @@
 const String APP_TITLE = 'Список интересных мест';
 const String FIRST_SCREEN_TITLE = 'Список\nинтересных мест';
+const String FIRST_SCREEN_TITLE_1 = 'С';
+const String FIRST_SCREEN_TITLE_2 = 'писок\n';
+const String FIRST_SCREEN_TITLE_3 = 'и';
+const String FIRST_SCREEN_TITLE_4 = 'нтересных ';
+const String FIRST_SCREEN_TITLE_5 = 'м';
+const String FIRST_SCREEN_TITLE_6 = 'ест';
 const String APP_WILL_BE_HERE= 'Здесь будет наше приложение';
--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -18,16 +18,31 @@ class _SightListScreenState extends State<SightListScreen> {
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(100.0),
         child: AppBar(
-          toolbarHeight: double.infinity,
-          title: Text(widget.title,
+            toolbarHeight: double.infinity,
+            title: RichText(
               textAlign: TextAlign.left,
-              style: TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.w700,
-              )),
-          backgroundColor: Colors.white,
-          elevation: 0,
-        ),
+              text: TextSpan(
+                  style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.w700,
+                      color: Color(0xFF3B3E5B)),
+                  children: [
+                    TextSpan(
+                        text: FIRST_SCREEN_TITLE_1,
+                        style: TextStyle(color: Colors.green)),
+                    TextSpan(text: FIRST_SCREEN_TITLE_2),
+                    TextSpan(
+                        text: FIRST_SCREEN_TITLE_3,
+                        style: TextStyle(color: Colors.yellow)),
+                    TextSpan(text: FIRST_SCREEN_TITLE_4),
+                    TextSpan(
+                        text: FIRST_SCREEN_TITLE_5,
+                        style: TextStyle(color: Colors.black)),
+                    TextSpan(text: FIRST_SCREEN_TITLE_6)
+                  ]),
+            ),
+            backgroundColor: Colors.white,
+            elevation: 0),
       ),
       body: Center(child: Text(APP_WILL_BE_HERE)),
     );
