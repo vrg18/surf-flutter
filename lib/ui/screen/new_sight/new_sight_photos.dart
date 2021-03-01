b33d4fb17085c4561c5e884ccbb3d6cd92b80190
valerii.grigorev <valerii.grigorev@simbirsoft.com>
2021-03-01 08:16:17 +0400

    drag-and-drop sort

--- lib/ui/screen/new_sight/new_sight_photos.dart
+++ lib/ui/screen/new_sight/new_sight_photos.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:places/ui/res/colors.dart';
 import 'package:places/ui/res/sizes.dart';
 
+/// Горизонтальный список с фотками нового места
 class NewSightPhotos extends StatefulWidget {
   final List<String> _photos;
 
@@ -14,59 +15,81 @@ class NewSightPhotos extends StatefulWidget {
 }
 
 class _NewSightPhotosState extends State<NewSightPhotos> {
+  final Map<String, bool> _isDrag = {};
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(basicBorderSize),
-      child: Row(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(right: basicBorderSize),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(cornerRadiusOfSightCard),
-              child: Material(
-                type: MaterialType.transparency,
-                child: InkWell(
-                  onTap: () => _addPhoto(),
-                  child: Container(
-                    width: photoSizeOfNewSight,
-                    height: photoSizeOfNewSight,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: bigGreenButtonColor,
-                      ),
-                      borderRadius: BorderRadius.circular(cornerRadiusOfSightCard),
-                    ),
-                    child: Icon(
-                      Icons.add,
-                      size: 48,
-                      color: bigGreenButtonColor,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Wrap(
-                direction: Axis.horizontal,
-                spacing: basicBorderSize,
-                children: widget._photos.map((e) => _photoContainer(e)).toList(),
-              ),
-            ),
-          ),
-        ],
+      child: SizedBox(
+        height: photoSizeOfNewSight,
+        child: ListView.separated(
+          scrollDirection: Axis.horizontal,
+          shrinkWrap: true,
+          itemCount: widget._photos.length + 1,
+          itemBuilder: (_, index) {
+            if (index == 0) return _largeAddButton();
+            return _photoContainer(index - 1);
+          },
+          separatorBuilder: (_, index) => _dragTarget(index),
+        ),
       ),
     );
   }
 
-  Widget _photoContainer(String photo) {
+  // Кнопка добавления фото
+  Widget _largeAddButton() {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(cornerRadiusOfSightCard),
+      child: Material(
+        type: MaterialType.transparency,
+        child: InkWell(
+          onTap: () => _addPhoto(),
+          child: Container(
+            width: photoSizeOfNewSight,
+            height: photoSizeOfNewSight,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: bigGreenButtonColor,
+              ),
+              borderRadius: BorderRadius.circular(cornerRadiusOfSightCard),
+            ),
+            child: Icon(
+              Icons.add,
+              size: 48,
+              color: bigGreenButtonColor,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Draggable над фото
+  Widget _photoContainer(int index) {
+    String _photo = widget._photos[index];
+    UniqueKey _key = UniqueKey();
+    if (!_isDrag.containsKey(_photo)) _isDrag[_photo] = false;
+
+    return Draggable<int>(
+      data: index,
+      axis: Axis.horizontal,
+      onDragStarted: () => setState(() => _isDrag[_photo] = true),
+      onDragEnd: (value) {
+        setState(() => _isDrag[_photo] = false);
+        print(value);
+      },
+      child: _isDrag[_photo]! ? const SizedBox(width: photoSizeOfNewSight) : _onePhoto(_key, index, _photo),
+      feedback: _onePhoto(_key, index, _photo),
+    );
+  }
+
+  // Само фото
+  Widget _onePhoto(UniqueKey key, int index, String photo) {
     return Dismissible(
-      key: UniqueKey(),
+      key: key,
       direction: DismissDirection.vertical,
-      onDismissed: (_) => _deletePhoto(photo),
+      onDismissed: (_) => _deletePhoto(index),
       child: Stack(
         children: [
           Container(
@@ -86,7 +109,7 @@ class _NewSightPhotosState extends State<NewSightPhotos> {
               child: SizedBox(
                 width: 48,
                 child: MaterialButton(
-                  onPressed: () => _deletePhoto(photo),
+                  onPressed: () => _deletePhoto(index),
                   materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
                   height: 48,
                   child: Icon(
@@ -102,12 +125,33 @@ class _NewSightPhotosState extends State<NewSightPhotos> {
     );
   }
 
+  // Виджет-приемник при перетаскивании
+  Widget _dragTarget(int destination) {
+    return DragTarget<int>(
+      builder: (_, __, ___) => SizedBox(
+        width: basicBorderSize,
+      ),
+      onAccept: (source) => _movePhoto(source, source >= destination ? destination : destination - 1),
+    );
+  }
+
+  // Добавление фото в список
   void _addPhoto() {
     setState(() =>
         widget._photos.add(Random().nextInt(0xFFFFFF).toRadixString(16))); // todo временно цветная заглушка вместо фото
   }
 
-  void _deletePhoto(String photo) {
-    setState(() => widget._photos.remove(photo));
+  // Удаление фото из списка
+  void _deletePhoto(int index) {
+    setState(() => widget._photos.removeAt(index));
+  }
+
+  // Перемещение фото в списке
+  void _movePhoto(int source, int destination) {
+    setState(() {
+      String photo = widget._photos[source];
+      widget._photos.removeAt(source);
+      widget._photos.insert(destination, photo);
+    });
   }
 }
